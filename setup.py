1645a95037427631cd56418e15ecd1bd0e3ed23f
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-02-23 11:57:17 +0100

    Setup explicit reqs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #from distutils.core import setup
 import re
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+# from pip.req import parse_requirements
 
 VERSIONFILE = "tweepy/__init__.py"
 ver_file = open(VERSIONFILE, "rt").read()
@@ -14,8 +14,12 @@ if mo:
 else:
     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
-install_reqs = parse_requirements('requirements.txt')
-reqs = [str(req.req) for req in install_reqs]
+# install_reqs = parse_requirements('requirements.txt')
+# reqs = [str(req.req) for req in install_reqs]
+reqs = [
+  'requests>=2.0.0',
+  'requests_oauthlib==0.4.0'
+]
 
 setup(name="tweepy",
       version=version,
